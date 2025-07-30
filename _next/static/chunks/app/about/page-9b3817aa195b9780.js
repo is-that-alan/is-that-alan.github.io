da6e420a053cc18081e67b973128efebaad83bb1
is-that-alan <is-that-alan@users.noreply.github.com>
2025-07-30 15:40:21 +0000

    deploy: d279eb24ac1ca2f5b74e9cb44c611741ce01f037

---
+++ _next/static/chunks/app/about/page-9b3817aa195b9780.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220],{7820:(e,s,n)=>{Promise.resolve().then(n.bind(n,5368)),Promise.resolve().then(n.bind(n,5653)),Promise.resolve().then(n.t.bind(n,6874,23)),Promise.resolve().then(n.t.bind(n,3063,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[797,332,441,684,358],()=>s(7820)),_N_E=e.O()}]);
